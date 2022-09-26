cf4ae13a2a7363f629805e4551a11332eea06898
Ilya <foreyk51@gmail.com>
2022-09-26 18:16:17 +0300

    Первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+
+
+    }
+
+    //Задание 1,2,3
+
+    private static void task1() {
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println(fullName);
+        fullName = fullName.replace('ё', 'е');
+        fullName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета —  " + fullName);
+
+
+
+    }
+
+
+    //Задание 2
+    private static void task2() {
+
     }
 }
